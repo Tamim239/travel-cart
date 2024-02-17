a1a59c1f3ef03cd4f58d44a37cdc44eda3b6ba39
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-02-17 12:25:08 +0600

    added grand total calculate

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
 const btns = document.getElementsByClassName("btns");
 let count = 0;
-for(const btn of btns){
-    btn.addEventListener("click", function(event){
-       count++;
-      
-    //  console.log(event.target.parentNode.parentNode.childNodes[1].innerText)
+for (const btn of btns) {
+    btn.addEventListener("click", function (event) {
+        count++;
 
-    //  access price and place
-    const place = event.target.parentNode.parentNode.childNodes[1].innerText;
-     const price = event.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
-    //  console.log(place, price)
-    //  create li and set innerText p, p2
-    const ul = document.getElementById("ul-container");
-    const li = document.createElement("li");
-    const p = document.createElement("p");
-    p.innerText = place;
-    const p2 = document.createElement("p");
-    p2.innerText = price;
-    li.appendChild(p);
-    li.appendChild(p2);
-    ul.appendChild(li);
-    
+        //  console.log(event.target.parentNode.parentNode.childNodes[1].innerText)
 
-    textSetValue("add-cart", count)
+        //  access price and place
+        const place = event.target.parentNode.parentNode.childNodes[1].innerText;
+        const price = event.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+        //  console.log(place, price)
+        //  create li and set innerText p, p2
+        const ul = document.getElementById("ul-container");
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.innerText = place;
+        const p2 = document.createElement("p");
+        p2.innerText = price;
+        li.appendChild(p);
+        li.appendChild(p2);
+        ul.appendChild(li);
+
+        // total cost access 
+        const totalCost = textInner("total-cost");
+        const convertTotalCost = parseInt(totalCost);
+        document.getElementById("total-cost").innerText = convertTotalCost + parseInt(price);
+          
+        // grand total access
+        const grandTotal = document.getElementById("grand-total");
+        const convertGrandTotal = parseInt(grandTotal.innerText);
+        console.log(convertGrandTotal);
+        document.getElementById("grand-total").innerText = convertGrandTotal + parseInt(price);
+          
+        
+
+        
+
+        textSetValue("add-cart", count)
     })
 
 }
--- js/utilities.js
+++ js/utilities.js
@@ -3,3 +3,24 @@ function textSetValue(elements, value){
     const element = document.getElementById(elements);
     element.innerText = value;
 }
+// set inner text
+function textInner(elements){
+    const element = document.getElementById(elements);
+    const text = element.innerText;
+    return text;
+}
+// grand total cost
+function grandTotalCost(){
+    const grandTotal = document.getElementById("grand-total");
+    const convertGrandTotal = parseInt(grandTotal.innerText);
+    console.log(convertGrandTotal);
+    document.getElementById("grand-total").innerText = convertGrandTotal + parseInt(price);
+}
+function selectBtn(e){
+    if(e === 'bus'){
+        parseInt(price) + 100;
+    }
+    else if(e === 'train'){
+        convertGrandTotal - 200;
+    }
+}
